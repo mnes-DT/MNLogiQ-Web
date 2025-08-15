fda254cbc0b650169f2fa5b3c786e503a8871993
mnes-DT <mnes-DT>
2025-08-15 20:45:32 +0530

    added contact form api of cadrush

--- scripts/components.js
+++ scripts/components.js
@@ -1,141 +1,141 @@
-
-
 class Dropdown {
-    constructor(selector, onChange) {
-        this.dropdown = document.querySelector(selector)
-        this.toggleButton = this.dropdown.querySelector('.dropdown-toggle')
-        this.onChange = onChange
+  constructor(selector, onChange) {
+    this.dropdown = document.querySelector(selector);
+    this.toggleButton = this.dropdown.querySelector(".dropdown-toggle");
+    this.onChange = onChange;
 
-        this.defaultText = this.toggleButton.querySelector("span").innerText
+    this.defaultText = this.toggleButton.querySelector("span").innerText;
 
-        this.menu = this.dropdown.querySelector('.dropdown-menu')
-        
-        this.selectItem = this.selectItem.bind(this)
-        this.toggleButton.addEventListener('click', this.toggleDropdown.bind(this))
-        document.addEventListener('click', this.closeDropdown.bind(this))
+    this.menu = this.dropdown.querySelector(".dropdown-menu");
 
-        this.lists = this.dropdown.querySelectorAll('li')
-        this.lists.forEach(e => {
-            e.addEventListener("click", () => this.selectItem(e))}
-        )
+    this.selectItem = this.selectItem.bind(this);
+    this.toggleButton.addEventListener("click", this.toggleDropdown.bind(this));
+    document.addEventListener("click", this.closeDropdown.bind(this));
 
-        this.value = ""
+    this.lists = this.dropdown.querySelectorAll("li");
+    this.lists.forEach((e) => {
+      e.addEventListener("click", () => this.selectItem(e));
+    });
 
-        this.dropDownInput = this.dropdown.querySelector(".dropdown-input")
+    this.value = "";
 
-    }
-  
-    toggleDropdown() {
-        this.menu.style.display = (this.menu.style.display === 'block') ? 'none' : 'block'
-    }
+    this.dropDownInput = this.dropdown.querySelector(".dropdown-input");
+  }
 
-    selectItem(ele){
+  toggleDropdown() {
+    this.menu.style.display =
+      this.menu.style.display === "block" ? "none" : "block";
+  }
 
-        const selectedInput = this.toggleButton.querySelector(".dropdown-select-text")
-        const selectIcon =  this.toggleButton.querySelector(".dropdown-select-icon")
+  selectItem(ele) {
+    const selectedInput = this.toggleButton.querySelector(
+      ".dropdown-select-text"
+    );
+    const selectIcon = this.toggleButton.querySelector(".dropdown-select-icon");
 
-        this.value = ele.querySelector(".dropdown-text").innerText.trim()
+    this.value = ele.querySelector(".dropdown-text").innerText.trim();
 
-        if (selectIcon && ele.querySelector(".dropdown-menu-icon")){
-            selectIcon.style.visibility = ""
-            selectIcon.setAttribute("src", ele.querySelector(".dropdown-menu-icon").src)
-            selectIcon.setAttribute("alt", ele.innerText)
-        }else{
-            selectIcon.style.visibility = "hidden"
-        }
-
-        selectedInput.innerText = ele.querySelector(".dropdown-text").innerText.trim()
-        
-        if(this.dropDownInput)
-            this.dropDownInput.value = this.value
-
-        if (this.onChange){
-            this.onChange(this.value)
-        }
-
-        this.closeDropdown()
-
+    if (selectIcon && ele.querySelector(".dropdown-menu-icon")) {
+      selectIcon.style.visibility = "";
+      selectIcon.setAttribute(
+        "src",
+        ele.querySelector(".dropdown-menu-icon").src
+      );
+      selectIcon.setAttribute("alt", ele.innerText);
+    } else {
+      selectIcon.style.visibility = "hidden";
     }
 
-    closeDropdown(event) {
-        if (event === undefined || !this.dropdown.contains(event.target)) {
-            this.menu.style.display = 'none'
-        }
-        document.removeEventListener('click', this.closeDropdown.bind(this))
+    selectedInput.innerText = ele
+      .querySelector(".dropdown-text")
+      .innerText.trim();
+
+    if (this.dropDownInput) this.dropDownInput.value = this.value;
+
+    if (this.onChange) {
+      this.onChange(this.value);
     }
+
+    this.closeDropdown();
+  }
+
+  closeDropdown(event) {
+    if (event === undefined || !this.dropdown.contains(event.target)) {
+      this.menu.style.display = "none";
+    }
+    document.removeEventListener("click", this.closeDropdown.bind(this));
+  }
 }
 
+class Prompt {
+  constructor(target) {
+    this.playground = document.querySelector(target);
 
-class Prompt{
+    this.promptWindow = this.playground.querySelector(".prompt-container");
 
-    constructor(target){
-        this.playground = document.querySelector(target)
+    this.chatModel = "gpt 4o";
 
-        this.promptWindow = this.playground.querySelector(".prompt-container")
-        
-        this.chatModel = "gpt 4o"
+    this.promptList = [];
 
-        this.promptList = []
+    this.answer = this.answer.bind(this);
+    this.addPrompt = this.addPrompt.bind(this);
+    this.setAIModel = this.setAIModel.bind(this);
+  }
 
-        this.answer = this.answer.bind(this)
-        this.addPrompt = this.addPrompt.bind(this)
-        this.setAIModel = this.setAIModel.bind(this)
+  setAIModel(model) {
+    // console.log("model: ", model)
+    this.chatModel = model.toLowerCase();
+  }
 
-    }
+  addPrompt(msg) {
+    if (this.promptList.length === 0) this.promptWindow.innerHTML = "";
 
-    setAIModel(model){
-        // console.log("model: ", model)
-        this.chatModel = model.toLowerCase()
-    }
+    this.promptList.push(msg);
 
-    addPrompt(msg){
+    const text = document.createElement("div");
 
-        if (this.promptList.length === 0)
-            this.promptWindow.innerHTML = ""
+    text.classList.add(
+      "tw-w-fit",
+      "tw-ml-auto",
+      "tw-p-2",
+      "tw-rounded-xl",
+      "tw-bg-gray-100",
+      "dark:tw-bg-[#171717]"
+    );
+    text.innerText = msg;
 
-        this.promptList.push(msg)
-
-        const text = document.createElement("div")
-
-        text.classList.add("tw-w-fit", "tw-ml-auto", "tw-p-2", "tw-rounded-xl", 
-                            "tw-bg-gray-100", "dark:tw-bg-[#171717]")
-        text.innerText = msg
-
-        const promptELement = `
+    const promptELement = `
             <div class="tw-w-full tw-flex tw-p-2">
                 ${text.outerHTML.toString()}
             </div>
-        `
+        `;
 
-        this.promptWindow.innerHTML += promptELement
+    this.promptWindow.innerHTML += promptELement;
 
-        setTimeout(() => {
-            this.promptWindow.scrollTop = this.promptWindow.scrollHeight
-        }, 150)
+    setTimeout(() => {
+      this.promptWindow.scrollTop = this.promptWindow.scrollHeight;
+    }, 150);
 
-        setTimeout(this.answer, 100)
-    }
+    setTimeout(this.answer, 100);
+  }
 
-    answer(){
+  answer() {
+    let msg = {
+      "gpt 4o": "Hello from Gpt 4o, add 3 prompts",
+      gemini: "Hello from Gemini, add 3 prompts",
+      "llama 3": "Hello from Meta Llama 3, add 3 prompts",
+      claude: "Hello from Claude, add 3 prompts",
+    }[this.chatModel];
 
-        let msg = {
-            "gpt 4o": "Hello from Gpt 4o, add 3 prompts",
-            "gemini": "Hello from Gemini, add 3 prompts",
-            "llama 3": "Hello from Meta Llama 3, add 3 prompts",
-            "claude": "Hello from Claude, add 3 prompts",
-        }[this.chatModel]
+    const text = document.createElement("div");
+    text.classList.add("tw-w-fit", "tw-mr-auto", "tw-p-2");
+    text.innerText = msg;
 
-        const text = document.createElement("div")
-        text.classList.add("tw-w-fit", "tw-mr-auto", "tw-p-2")
-        text.innerText = msg
-
-        const promptELement = `
+    const promptELement = `
             <div class="tw-w-full tw-flex tw-p-2">
                 ${text.outerHTML.toString()}
             </div>
-        `
-        this.promptWindow.innerHTML += promptELement
-
-    }
-
+        `;
+    this.promptWindow.innerHTML += promptELement;
+  }
 }
