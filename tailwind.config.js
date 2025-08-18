4d1be90a7998e1fc5454e8ada18926870e8a7d34
lakshmi <you@example.com>
2025-08-18 15:28:52 +0530

    logo changes & contact number

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  prefix: "tw-",
-  important: false,
-  content: ["**/*.{html, jsx, js}", "**/*.js", "**/*.html"],
-  darkMode: "class",
-  theme: {
-    extend: {
-      fontFamily: {
-        poly: ['"poly"', "serif"],
-      },
-    },
-  },
-  plugins: [
-    function ({ addVariant }) {
-      addVariant("firefox", ":-moz-any(&)");
-    },
-  ],
+	prefix: "tw-",
+	important: false,
+	content: ["**/*.{html, jsx, js}", "**/*.js", "**/*.html"],
+	darkMode: "class",
+	theme: {
+		extend: {
+			fontFamily: {
+				poly: ['"poly"', "serif"],
+			},
+		},
+	},
+	plugins: [
+		function ({ addVariant }) {
+			addVariant("firefox", ":-moz-any(&)");
+		},
+	],
 };
